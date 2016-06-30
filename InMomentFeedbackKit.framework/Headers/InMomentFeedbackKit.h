8266697ec39350a334d4037141144e8dda152ce9
Kyler Jensen <kjensen@inmoment.com>
2016-06-29 18:08:06 -0600

    Deployed version 1.0

---
+++ InMomentFeedbackKit.framework/Headers/InMomentFeedbackKit.h
@@ -0,0 +1,19 @@
+//
+//  IMFeedbackKit.h
+//  IMFeedbackKit
+//
+//  Created by Kyler Jensen on 6/24/16.
+//  Copyright © 2016 InMoment, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IMFeedbackKit.
+FOUNDATION_EXPORT double InMomentFeedbackKitVersionNumber;
+
+//! Project version string for IMFeedbackKit.
+FOUNDATION_EXPORT const unsigned char InMomentFeedbackKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IMFeedbackKit/PublicHeader.h>
+
+
